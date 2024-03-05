f715ad4cbb0880dca3b56afcaffaf1881a141040
Edwardd02 <tup19197@temple.edu>
2024-03-05 18:09:02 -0500

    Class Bidder initialized

--- Bidder.m
+++ Bidder.m
@@ -2,8 +2,12 @@ classdef Bidder
     properties
         bidderID double
         budget double
-        currentBid double = 0 % psychological price point
-        maxBid double % 
+        currentBid double = 0 
+        % the individual's psychological valuation for the auction item in 
+        % the current bidding session
+        maxBid double 
+        % the individual's maximum psychological bidding threshold for the 
+        % auction lot
         strategy BiddingStrategy
     end
     
@@ -18,7 +22,8 @@ classdef Bidder
         
         % Method to place a bid
         function obj = placeBid(obj, currentHighestBid)
-            obj.currentBid = obj.strategy.generateBid(currentHighestBid, obj.maxBid, obj.budget);
+            obj.currentBid = obj.strategy.generateBid(currentHighestBid,...
+                obj.maxBid, obj.budget);
         end
         
         % Additional methods as needed
