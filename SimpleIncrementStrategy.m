e67d8fe0e1a093f2984ca23c0c9596655aeaa5a5
Edwardd02 <tup19197@temple.edu>
2024-04-02 15:08:49 -0400

    Sniping Strategy Added

---
+++ SimpleIncrementStrategy.m
@@ -0,0 +1,19 @@
+classdef SimpleIncrementStrategy < BiddingStrategy
+    properties
+        incrementFactor double;
+    end
+    
+    methods
+        function obj = SimpleIncrementStrategy(incrementFactor)
+            obj.incrementFactor = incrementFactor;
+        end
+        function bid = generateBid(obj, currentBid, maxBid, budget, minIncrement, roundsToLast) % Unrelated arguments: roundToLast
+            proposedBid = currentBid * (1 + obj.incrementFactor);
+            if proposedBid - currentBid > minIncrement
+                bid = min([proposedBid, maxBid, budget]);
+            else
+                bid = currentBid;
+            end
+        end
+    end
+end
---
+++ SnipingStrategy.m
@@ -0,0 +1,30 @@
+classdef SnipingStrategy < BiddingStrategy
+    properties
+        snipeTiming % How close to the end of the auction to place the bid
+    end
+    
+    methods
+        function obj = SnipingStrategy(snipeTiming)
+            % Constructor for SnipingStrategy
+            % snipeTiming should be a value indicating how many rounds from the end to place the bid
+            obj.snipeTiming = snipeTiming;
+        end
+        function bid = generateBid(obj, currentBid, maxBid, budget, minIncrement, roundsToLast)
+            % Generate a bid based on the sniping strategy
+            if roundsToLast <= obj.snipeTiming
+                % TODO: when last round is getting closer and closer, the
+                % weight should be getting larger and larger
+                % Time to snipe! Check if we can outbid by at least the minimum increment
+                if (currentBid + minIncrement <= budget) && (currentBid + minIncrement <= maxBid)
+                    bid = currentBid + minIncrement;
+                else
+                    % Can't bid or don't need to bid yet
+                    bid = 0;
+                end
+            else
+                % Not time to bid yet
+                bid = 0;
+            end
+        end
+    end
+end
